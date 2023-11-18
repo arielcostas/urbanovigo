6dfe197679aa16110c61e0ee69631bb9b6553f00
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-11-18 14:13:46 +0100

    Update hosting scripts

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ git pull
 
 sudo systemctl stop infobus-bot.service
 
-dotnet publish --output /opt/bot src/BotVitrasa.csproj --self-contained true -p Release
+dotnet publish --output /opt/bot Vigo360.InfobusBot/Vigo360.InfobusBot.csproj --self-contained true -p Release
 
 sudo systemctl start infobus-bot.service
